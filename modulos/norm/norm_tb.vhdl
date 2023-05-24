d72a20d1b5fd3cbe7f1e36391f9da442387e2e4b
Luís Otávio Amorim <luisotaviolamorim@gmail.com>
2023-05-24 10:05:19 -0300

    edit: Mudanças no módulo de normalização para operar com vetores
    Antes o módulo de normalização recebia um número em ponto fixo e dois
    limites de intervalo. Agora ele recebe um vetor de números de ponto
    fixo, que represena as características de uma dada amostra de teste, e
    um vetor de mínimos e de máximos de cada característica, colocando na
    saída o resultado da normalização de todas as características.
    
    Assim fica mais simples normalizar vetores, já que antes era necessário
    normalizar o vetor elemento a elemento.
     Changes to be committed:
    	modified:   gtkw/norm.gtkw
    	modified:   modulos/norm/norm.vhdl
    	modified:   modulos/norm/norm_tb.vhdl

--- modulos/norm/norm_tb.vhdl
+++ modulos/norm/norm_tb.vhdl
@@ -1,7 +1,7 @@
 library ieee;
 use ieee.fixed_pkg.all;
-use std.textio.all;
 use std.env.finish;
+use ieee.math_real.all;
 
 library work;
 use work.pacote_aux.all;
@@ -9,31 +9,74 @@ entity norm_tb is
 end norm_tb;
 
 architecture sim of norm_tb is
-    signal entrada_ponto_fixo : s_fixo := s_fixo_min;
 
     -- portas do componente
-    signal resultado : s_fixo;
+    signal i_clk       : bit := '0';
+    signal i_init      : bit := '0';
+    signal i_reset     : bit := '0';
+    signal o_ocupado   : bit;
+    signal i_x         : vec_s_fixo(n_caracteristicas - 1 downto 0);
+    signal o_x_norm    : vec_s_fixo(n_caracteristicas - 1 downto 0);
+
+    -- contador de ciclos de clock
+    signal contador            : integer := 0;
+    signal conta_normalizacoes : integer := 0;
 
 
 begin
 
     UUT : entity work.norm
         generic map(
-            gen_max_x => s_fixo_max,
-            gen_min_x => s_fixo_min
+            gen_n_caracteristicas => n_caracteristicas
         )
-        port map(
-            i_x      => entrada_ponto_fixo,
-            o_x_norm => resultado
+        port map
+        (
+            i_clk       => i_clk,
+            i_init      => i_init,
+            i_reset     => i_reset,
+            o_ocupado   => o_ocupado,
+            i_x         => i_x,
+            i_min_x     => menor_por_caracteristica,
+            i_max_x     => maior_por_caracteristica,
+            o_x_norm    => o_x_norm
         );
 
     clock : process
     begin
-        entrada_ponto_fixo <= resize(arg => entrada_ponto_fixo + s_fixo_lsb,
-            size_res                         => entrada_ponto_fixo);
-        wait for 1 ns;
-        if entrada_ponto_fixo = s_fixo_max then
-            finish;
-        end if;
+        i_clk <= not i_clk;
+        wait for 400 ns; -- 50Mhz
     end process clock;
+
+    inicia : process
+        -- Variáveis do gerador de números aleatórios
+        variable seed1 : positive := 15646526;
+        variable seed2 : positive := 54612348;
+        variable rand  : real;
+    begin
+
+        -- popula vetor de entradas
+        for i in n_caracteristicas - 1 downto 0 loop
+            uniform(seed1, seed2, rand);
+            i_x(i) <= resize(rand * maior_por_caracteristica(i) + menor_por_caracteristica(i), i_x(i)); -- quase sempre entre menor e maior
+            wait for 10 ns;
+        end loop;
+
+        i_init <= '1';
+        wait for 1 us;
+        i_init <= '0';
+        wait for 2 us;
+        finish;
+    end process inicia;
+
+
+    process (o_ocupado, i_clk)
+    begin
+        if falling_edge(o_ocupado) or rising_edge(o_ocupado) then
+            contador <= 0;
+        elsif rising_edge(i_clk) then
+            contador <= contador + 1;
+        end if;
+
+    end process;
+
 end architecture sim;
